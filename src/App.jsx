02a7b1797e43a01136f6db6dc1b09da4ed8f03b7
hunii ali <huniiali1122@gmail.com>
2025-10-01 23:49:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LocomotiveScroll from 'locomotive-scroll';
+
+const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+
+  return (
+    <div>
+      <h3>hey hunain</h3>
+      
+    </div>
+  )
+}
+
+export default App
